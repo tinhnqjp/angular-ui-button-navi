421728e36ac8710394ca38b14ce730238de580ad
tinhnqjp <tinhnqjp@gmail.com>
2019-05-10 16:25:34 +0900

    the first

---
+++ index.js
@@ -0,0 +1,21 @@
+angular.module('core').directive('buttonBack', buttonBack);
+buttonBack.$inject = ['$state'];
+function buttonBack($state) {
+    var directive = {
+    restrict: 'E',
+    scope: {
+        back: '@',
+        params: '='
+    },
+    templateUrl: '/template/button-back.client.view.html',
+    link: function (scope) {
+        scope.handleBackScreen = function () {
+        if (scope.back) {
+            $state.go(scope.back, scope.params);
+        }
+        };
+    }
+    };
+
+    return directive;
+}
