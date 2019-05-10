98f0c20d52d8bed4cb73145a0a6110d4317dfd03
tinhnqjp <tinhnqjp@gmail.com>
2019-05-10 18:07:55 +0900

    commit

--- index.js
+++ index.js
@@ -15,7 +15,56 @@ angular.module('ui.button-navi', [])
         };
       }
     };
-
+    return directive;
+  }])
+  .directive('buttonEdit', ['$state', function ($state) {
+    var directive = {
+      restrict: 'E',
+      scope: {
+        nohistory: '@',
+        back: '@',
+        noback: '@',
+        notpre: '=',
+        state: '@',
+        params: '='
+      },
+      templateUrl: 'template/button-edit.client.view.html',
+      link: function (scope) {
+        scope.handleBackScreen = function () {
+            if (scope.nohistory) {
+              $state.go(scope.back, scope.params);
+            } else if ($state.previous.state.name) {
+              if (scope.state === $state.previous.state.name || scope.noback === $state.previous.state.name) {
+                $state.go(scope.back, scope.params);
+              } else {
+                $state.go($state.previous.state.name, ($state.previous.state.name) ? $state.previous.params : {});
+              }
+            } else if (scope.back) {
+              $state.go(scope.back, scope.params);
+            }
+          };
+      }
+    };
+    return directive;
+  }])
+  .directive('buttonSave', ['$state', function ($state) {
+    var directive = {
+      restrict: 'E',
+      scope: {
+        back: '@',
+        params: '='
+      },
+      templateUrl: 'template/button-save.client.view.html',
+      link: function (scope) {
+        scope.handleBackScreen = function () {
+            if ($state.previous.state.name) {
+              $state.go($state.previous.state.name, ($state.previous.state.name) ? $state.previous.params : {});
+            } else if (scope.back) {
+              $state.go(scope.back, scope.params);
+            }
+          };
+      }
+    };
     return directive;
   }]);
 
@@ -24,6 +73,12 @@ angular.module('ui.button-navi').run(['$templateCache', function ($templateCache
 
   $templateCache.put('template/button-back.client.view.html',
     '<button type="button" ng-click="handleBackScreen()" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i>戻る</button>'
+  );
+  $templateCache.put('template/button-edit.client.view.html',
+    '<button type="button" ng-click="handleBackScreen()" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i>戻る</button><button class="btn btn-primary btn-sm" ui-sref="{{state}}({{params}})" title="編集"><i class="fa fa-edit"></i>編集</button>'
+  );
+  $templateCache.put('template/button-save.client.view.html',
+    '<button type="button" ng-click="handleBackScreen()" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i>戻る</button><button type="submit" class="btn btn-success btn-sm"><i class="fa fa-save"></i>保存</button>'
   );
 
 }]);
