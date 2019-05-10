0d2fa38bb09cca8af130e2b982ab98f533d79bb5
tinhnqjp <tinhnqjp@gmail.com>
2019-05-10 17:40:21 +0900

    commit

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-angular.module('ui.button-navi').directive('buttonBack', buttonBack);
-buttonBack.$inject = ['$state'];
-function buttonBack($state) {
+angular.module('ui.button-navi', [])
+  .directive('buttonBack', ['$state', function ($state) {
     var directive = {
-    restrict: 'E',
-    scope: {
+      restrict: 'E',
+      scope: {
         back: '@',
         params: '='
-    },
-    templateUrl: '/template/button-back.client.view.html',
-    link: function (scope) {
+      },
+      templateUrl: 'template/button-back.client.view.html',
+      link: function (scope) {
         scope.handleBackScreen = function () {
-        if (scope.back) {
+          if (scope.back) {
             $state.go(scope.back, scope.params);
-        }
+          }
         };
-    }
+      }
     };
 
     return directive;
+  }]);
+
+angular.module('ui.button-navi').run(['$templateCache', function ($templateCache) {
+  'use strict';
+
+  $templateCache.put('template/button-back.client.view.html',
+    '<button type="button" ng-click="handleBackScreen()" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i>戻る</button>'
+  );
+
+}]);
+
+
+if (typeof exports === 'object' && typeof module === 'object') {
+  module.exports = 'ui.button-navi';
 }
